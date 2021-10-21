17fe63cc3d9efd2ae94b826f931b9bd696e84566
menghuiguli <fujia0@qq.com>
2021-10-21 20:59:23 +0800

    add package

--- front/src/App.js
+++ front/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 
 import Search from  './components/Search'
+import Nav from  './components/Nav'
+
 
 function App() {
   return (
     <div className="App">
     <div className="App-header">
+        <Nav />
      <Search />
      </div>
     </div>
